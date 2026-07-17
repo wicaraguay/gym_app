8367b1bacaccfe000f8b207664a4543be5c035a2
Willan Caraguay <wicaraguay@gmail.com>
2026-07-17 09:54:33 -0500

    fix: eliminacion de mobile

--- mobile/src/auth.ts
+++
@@ -1,9 +0,0 @@
-import * as SecureStore from 'expo-secure-store';
-
-// Guardamos el token de sesion de forma segura en el celular.
-export const saveToken = (token: string) =>
-  SecureStore.setItemAsync('token', token);
-
-export const getToken = () => SecureStore.getItemAsync('token');
-
-export const clearToken = () => SecureStore.deleteItemAsync('token');
